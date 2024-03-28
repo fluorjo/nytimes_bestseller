c540af5ac0cf5d1b2882d7897713db924bf5939a
fluorjo <fluorjo@naver.com>
2024-03-29 04:19:22 +0900

    다시 1차 완성

--- components/category.tsx
+++ components/category.tsx
@@ -8,41 +8,35 @@ export default async function Category() {
   const router = useRouter();
   const categoryList = await getCategory();
   return (
-    <div className={styles.container}>
-      <div className={styles.flex_column}>
-        <h2>asdkjhasdfkjhasdas2</h2>
-        <div className={styles.divided_container}>
-          {categoryList.results
-            .filter((l) => l.updated === "WEEKLY")
-            .map((l) => (
-              <li
-                key={l.list_name_encoded}
-                onClick={() => router.push(`/list/${l.list_name_encoded}`)}
-                style={{ cursor: "pointer" }}
-                className={styles.divided_container_li}
-              >
-                {l.display_name}
-              </li>
-            ))}
+      <div className={styles.container}>
+          <div className={styles.divided_container}>
+            {categoryList.results
+              .filter((l) => l.updated === "WEEKLY")
+              .map((l) => (
+                <li
+                  key={l.list_name_encoded}
+                  onClick={() => router.push(`/list/${l.list_name_encoded}`)}
+                  style={{ cursor: "pointer" }}
+                  className={styles.divided_container_li}
+                >
+                  {l.display_name}
+                </li>
+              ))}
+          </div>
+          <div className={styles.divided_container}>
+            {categoryList.results
+              .filter((l) => l.updated === "MONTHLY")
+              .map((l) => (
+                <li
+                  key={l.list_name_encoded}
+                  onClick={() => router.push(`/list/${l.list_name_encoded}`)}
+                  style={{ cursor: "pointer" }}
+                  className={styles.divided_container_li}
+                >
+                  {l.display_name}
+                </li>
+              ))}
+          </div>
         </div>
-      </div>
-      <div className={styles.flex_column}>
-        <h2>wdwdd</h2>
-        <div className={styles.divided_container}>
-          {categoryList.results
-            .filter((l) => l.updated === "MONTHLY")
-            .map((l) => (
-              <li
-                key={l.list_name_encoded}
-                onClick={() => router.push(`/list/${l.list_name_encoded}`)}
-                style={{ cursor: "pointer" }}
-                className={styles.divided_container_li}
-              >
-                {l.display_name}
-              </li>
-            ))}
-        </div>
-      </div>
-    </div>
   );
 }
