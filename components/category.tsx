922e4a75c4e5db7cc5eb65e4424527c2474f087e
fluorjo <fluorjo@naver.com>
2024-03-29 04:30:00 +0900

    주간, 월간 텍스트 넣기

--- components/category.tsx
+++ components/category.tsx
@@ -8,35 +8,45 @@ export default async function Category() {
   const router = useRouter();
   const categoryList = await getCategory();
   return (
-      <div className={styles.container}>
-          <div className={styles.divided_container}>
-            {categoryList.results
-              .filter((l) => l.updated === "WEEKLY")
-              .map((l) => (
-                <li
-                  key={l.list_name_encoded}
-                  onClick={() => router.push(`/list/${l.list_name_encoded}`)}
-                  style={{ cursor: "pointer" }}
-                  className={styles.divided_container_li}
-                >
-                  {l.display_name}
-                </li>
-              ))}
-          </div>
-          <div className={styles.divided_container}>
-            {categoryList.results
-              .filter((l) => l.updated === "MONTHLY")
-              .map((l) => (
-                <li
-                  key={l.list_name_encoded}
-                  onClick={() => router.push(`/list/${l.list_name_encoded}`)}
-                  style={{ cursor: "pointer" }}
-                  className={styles.divided_container_li}
-                >
-                  {l.display_name}
-                </li>
-              ))}
-          </div>
+    <div className={styles.container}>
+      <div className={styles.divided_container}>
+        <div>
+          <h2>WEEKLY</h2>
         </div>
+        <div className={styles.margin}>
+          {categoryList.results
+            .filter((l) => l.updated === "WEEKLY")
+            .map((l) => (
+              <li
+                key={l.list_name_encoded}
+                onClick={() => router.push(`/list/${l.list_name_encoded}`)}
+                style={{ cursor: "pointer" }}
+                className={styles.divided_container_li}
+              >
+                {l.display_name}
+              </li>
+            ))}
+        </div>
+      </div>
+      <div className={styles.divided_container}>
+      <div>
+          <h2>MONTHLY</h2>
+        </div>
+        <div className={styles.margin}>
+          {categoryList.results
+            .filter((l) => l.updated === "MONTHLY")
+            .map((l) => (
+              <li
+                key={l.list_name_encoded}
+                onClick={() => router.push(`/list/${l.list_name_encoded}`)}
+                style={{ cursor: "pointer" }}
+                className={styles.divided_container_li}
+              >
+                {l.display_name}
+              </li>
+            ))}
+        </div>
+      </div>
+    </div>
   );
 }
