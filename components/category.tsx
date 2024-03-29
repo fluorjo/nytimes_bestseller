cbe8dba6e9474ffc44a4270fea4104fe296e00a2
fluorjo <fluorjo@naver.com>
2024-03-29 18:41:20 +0900

    카테고리 화면에서 온클릭 불안정한 거 수정

--- components/category.tsx
+++ components/category.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import getCategory from "../api";
 
 import styles from "../styles/category.module.css";
 
-export default async function Category() {
+export default  function Category() {
   const router = useRouter();
-  const categoryList = await getCategory();
+  const [categoryList, setCategoryList] = useState({ results: [] });
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getCategory();
+      setCategoryList(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.divided_container}>
@@ -39,6 +49,7 @@ export default async function Category() {
               <li
                 key={l.list_name_encoded}
                 onClick={() =>router.push(`/list/${l.list_name_encoded}`)}
+                // onClick={() =>console.log('22222')}
 
                 style={{ cursor: "pointer" }}
                 className={styles.divided_container_li}
