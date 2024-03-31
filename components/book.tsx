5b5162fa205c1e9d02e4e4a97c98c532dae0ad91
fluorjo <fluorjo@naver.com>
2024-04-01 00:40:20 +0900

    버튼 css

--- components/book.tsx
+++ components/book.tsx
@@ -143,7 +143,7 @@ export default function Book({
       <div className={styles.bookInfoBox}>
         <h2>{title}</h2>
         <h3>by {author}</h3>
-        <button onClick={goBuyNow}>Buy Now!</button>
+        <button className={styles.BuyButton} onClick={goBuyNow}>Buy ↓ </button>
       </div>
     </div>
   );
